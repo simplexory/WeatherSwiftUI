c9d707878ef5a3bc6f74471972c0ebe408ad1739
Hankovich Yury <simplexory@gmail.com>
2023-08-22 21:40:15 +0300

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Юра Ганкович on 22.08.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
